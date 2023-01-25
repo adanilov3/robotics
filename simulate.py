96076fbbfa21234bcfaee7019b0ac0eedad1e4a8
sasha <adanilov@uvm.edu>
2023-01-25 14:34:57 -0500

    added for loop and time's sleep function

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
+
+x = 1000
+t = 1/60
+
 physicsClient = p.connect(p.GUI)
-for i in range(1000):
+for i in range(x):
+    time.sleep(t)
     p.stepSimulation()
+    print(i)
 p.disconnect()
