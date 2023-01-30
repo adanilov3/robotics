149d22062d9cfa9c034399730c6eb999ce926a5f
sasha <adanilov@uvm.edu>
2023-01-30 13:09:43 -0500

    torso

--- generate.py
+++ generate.py
@@ -1,11 +1,20 @@
 import pyrosim.pyrosim as pyrosim
-
-pyrosim.Start_SDF("world.sdf")
 z=0.5
 y=0
 x=0
 l=1
 w=1
 h=1
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[l,w,h])
-pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x+1,y+1,z] , size=[l,w,h])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[l,w,h])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
