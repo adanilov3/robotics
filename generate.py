c08acd6c56fe77b57278499f21bcb84450ce8162
sasha <adanilov@uvm.edu>
2023-01-30 14:05:04 -0500

    linked together but something is wrong

--- generate.py
+++ generate.py
@@ -8,12 +8,14 @@ h=1
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x+1,y+1,z] , size=[l,w,h])
+    pyrosim.Send_Cube(name="Box", pos=[1,1,0.5] , size=[1,1,1])
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[l,w,h])
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5], size= [1,1,1])
     pyrosim.End()
 
 Create_World()
