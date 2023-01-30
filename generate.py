97c0a457fa54425e1aa20fa4fabfeaffda49feb7
sasha <adanilov@uvm.edu>
2023-01-30 12:30:19 -0500

    castle!!!

--- generate.py
+++ generate.py
@@ -4,15 +4,21 @@ pyrosim.Start_SDF("boxes.sdf")
 z=0.5
 y=0
 x=0
-a=1
-b=1
-c=1
+l=1
+w=1
+h=1
 
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[a,b,c])
-    z+=1
-    a -= (a * 0.1)
-    b -= (b * 0.1)
-    c -= (c * 0.1)
-
+for m in range(5):
+    l = 1
+    w = 1
+    h = 1
+    for n in range(5):
+        l = 1
+        w = 1
+        h = 1
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x+m,y+n,z+i] , size=[l,w,h])
+            l -= (l * 0.1)
+            w -= (l * 0.1)
+            h -= (l * 0.1)
 pyrosim.End()
