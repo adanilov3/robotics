38c6d4a8d8bf8299bb54daa444a9029a9f9702c9
sasha <adanilov@uvm.edu>
2023-02-05 12:04:47 -0500

    added lines of code for robot to sense

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pybullet_data
 import time
+import pyrosim.pyrosim as pyrosim
 
 x = 1000
 t = 1/30
@@ -11,8 +12,10 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(x):
     time.sleep(t)
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     print(i)
 p.disconnect()
