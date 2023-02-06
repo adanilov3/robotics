58838c2ec18235833dc36699e434f88a689dd46e
sasha <adanilov@uvm.edu>
2023-02-06 14:29:51 -0500

    added matplotlib

--- analyze.py
+++ analyze.py
@@ -1,3 +1,10 @@
-import numpy
+import numpy as numpy
+import matplotlib.pyplot
 
+file = numpy.load('data/values.npy')
+backLegSensorValues = numpy.array(file)
+
+matplotlib.pyplot.plot(backLegSensorValues)
+
+matplotlib.pyplot.show()
 
